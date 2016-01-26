b330751f5b6993a892d6400b563d9ce2585a9fa1
Lena Hoplamazian <lhoplama@lsoc.org>
2016-01-26 13:29:25 -0600

    first draft of rp4, 1/26

--- rps2.rb
+++ rps2.rb
@@ -1,21 +1,33 @@
 
 #Set a variable for computer_win equal to 0
+computer_win = 0 
 #Set a variable for human_win equal to 0
+human_win = 0
 
-
-#Display “Welcome to RPS 2016. The winner gets rich, the loser dies.”
+#Display “Welcome to RPS 2016. The winner gets rich, the loser dies.”\
+puts "Welcome to RPS 2016. The winner gets rich, the loser dies"
 #Display “Rock...Paper...Scissors...Shoot!”
+puts "Rock... Paper... Scissors... Shoot!"
+
 
 #Display “Press 1 for rock, 2 for paper, 3 for scissors”
+puts "Press 1 for rock, 2 for paper, 3 for scissors"
+weapon.to_i = gets.chomp
 #Get user's choice, set to variable
 
 #Get computer's choice, select 1,2 or 3 randomly, set to a variable
-
+ [1,2,3,].sample 
+ compweapon = gets.chomp
 
 #Set Human_Win = 0
+human_win = 0
 #Set Comp_Win = 0
-
+computer_win = 0
 #Repeat until Human_Win or Comp_Win = 2
+if computer_win or human_win = 2
+
+if weapon - compweapon = 1 or weapon - compweapon = -2
+	puts "you win!"
     #if user - comp = 1 OR user - comp = -2 
     	#display “You win!” add 1 to Human_win
     # else
